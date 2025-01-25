22aec06ebf659b4f2a581870d1b2f541bb7d9730
Sjx0ck <tim.dimytch@gmail.com>
2025-01-24 17:10:41 -0800

    v2.0

--- Tim_Website/main/static/main/js/main.js
+++ Tim_Website/main/static/main/js/main.js
@@ -113,3 +113,51 @@ window.onload = function () {
 
     hidePreloader();
 };
+
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(img => {
+  img.addEventListener('click', () => {
+    const modal = document.querySelector(img.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.active-modal')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.active-modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) {
+    console.error('Modal not found');
+    return;
+  }
+  modal.classList.remove('modal')
+  modal.classList.add('active-modal');
+  overlay.classList.add('active-overlay');
+  console.log('Modal opened:', modal); // Диагностика
+}
+
+function closeModal(modal) {
+  if (modal == null) {
+    console.error('Modal not found');
+    return;
+  }
+  modal.classList.add('modal')
+  modal.classList.remove('active-modal');
+  overlay.classList.remove('active-overlay');
+  console.log('Modal closed:', modal); // Диагностика
+}
